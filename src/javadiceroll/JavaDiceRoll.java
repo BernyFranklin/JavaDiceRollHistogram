a2990a7c09b45cfb740bc7b8c718b39ddffa03dc
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-25 10:31:58 -0700

    Initial Commit for JavaDiceRoll

---
+++ src/javadiceroll/JavaDiceRoll.java
@@ -0,0 +1,24 @@
+/*
+ * JavaDiceRoll Version1.0
+ * Frank Bernal
+ * CIS 084 Java Programming
+ * Input: None
+ * Output: Histogram of 1,000 Dice Rolls
+ * Date
+ */
+
+package javadiceroll;
+
+// Import Math library for Math.random
+import java.lang.Math;
+
+public class JavaDiceRoll {
+
+    // Global stuff
+    
+    public static void main(String[] args) {
+        
+        // Local Stuff
+    }   // End of PSV Main
+    
+}   // End of Class
