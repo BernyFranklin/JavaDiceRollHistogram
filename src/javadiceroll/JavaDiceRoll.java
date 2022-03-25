3828832d2a1ceed240d3fd11baa674e89dee0aed
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-25 11:27:31 -0700

    Created display for final output.

--- src/javadiceroll/JavaDiceRoll.java
+++ src/javadiceroll/JavaDiceRoll.java
@@ -15,8 +15,10 @@ import java.lang.Math;
 public class JavaDiceRoll {
 
     // Global stuff
-    private static final int SIDES_ON_DIE = 6;
-    private static final int TOTAL_ROLLS = 1000;
+    private static final int SIDES_ON_DIE = 6;    // Using a 6 sided die
+    private static final int TOTAL_ROLLS = 1000;  // Project requires 1000 rolls
+    
+    // Start main
     public static void main(String[] args) {
         
         // Local variables
@@ -26,6 +28,8 @@ public class JavaDiceRoll {
         // Array to store results of 1,000 dice rolls
         int[] pointCount = new int[13];
         
+        // Roll each die for TOTAL_ROLLS
+        // Store the sum of every roll and update counter array
         for (int i = 0; i < TOTAL_ROLLS; i++) {
             // Using Math.random for random number
             // Multiplied by 6 (SIDES_ON_DIE) makes selection 0-5
@@ -34,14 +38,27 @@ public class JavaDiceRoll {
             dieTwo = (int)(Math.random()*SIDES_ON_DIE)+1;
             // Store sum of two dice
             sumOfDice = dieOne + dieTwo;
+            // Increment index of sum into pointCount[]
             pointCount[sumOfDice]++;
-        }
+        }   // End of for loop
         
-        // Test Print
-        System.out.println ("Roll       Total Rolls");
+        // Program Display
+        // Header
+        System.out.println ("=========================================");
+        System.out.printf ("This program rolls two dice %4d times\n", TOTAL_ROLLS);
+        System.out.println ("and displays how many times each outcome");
+        System.out.println ("is rolled.");
+        System.out.println ("=========================================");
+        System.out.printf ("Roll       Total in %d Rolls\n", TOTAL_ROLLS);
+        System.out.println ("=========================================");
+        
+        // Loop to print all outcomes using i for counter and index count
         for (int i = 2; i <=12; i++) {
-            System.out.printf ("%3d           %3d\n", i, pointCount[i]);
-        }
+            System.out.printf ("%3d                  %3d\n", i, pointCount[i]);
+        }   // End of loop
+        
+        // Line for formatting
+        System.out.println ("=========================================");
         
     }   // End of PSV Main
     
