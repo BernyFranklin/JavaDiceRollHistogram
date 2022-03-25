cb74a49159b4554bc8d28395893e2e5a12fd08ba
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-25 10:43:25 -0700

    Made SIDES_ON_DIE a constant of 6 Made variables for two dice generated randomly 1-6. Made variable for sum of two dice.

--- src/javadiceroll/JavaDiceRoll.java
+++ src/javadiceroll/JavaDiceRoll.java
@@ -15,10 +15,21 @@ import java.lang.Math;
 public class JavaDiceRoll {
 
     // Global stuff
-    
+    private static final int SIDES_ON_DIE = 6;
     public static void main(String[] args) {
         
-        // Local Stuff
+        // Local variables
+        // Using Math.random for random number
+        // Multiplied by 6 (SIDES_ON_DIE) makes selection 0-5
+        // Adding 1 makes the selection 1-6
+        int dieOne = (int)(Math.random()*SIDES_ON_DIE)+1;
+        int dieTwo = (int)(Math.random()*SIDES_ON_DIE)+1;
+        int sumOfDice = dieOne + dieTwo;
+        
+        // Test Print
+        System.out.printf ("Die one rolled: %2d\n", dieOne);
+        System.out.printf ("Die two rolled: %2d\n", dieTwo);
+        System.out.printf ("Sum of dice:    %2d\n", sumOfDice); 
     }   // End of PSV Main
     
 }   // End of Class
